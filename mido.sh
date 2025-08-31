852158024de8a4e9b24f39799b2b36708577cdd8
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-01 03:34:20 +0700

    Update mido.sh

--- mido.sh
+++ mido.sh
@@ -1,65 +1,148 @@
 #!/usr/bin/env bash
+
+# Dependencies
 rm -rf kernel
-git clone $REPO -b $BRANCH kernel 
+git clone $REPO -b $BRANCH kernel
 cd kernel
-echo "Nuke previous toolchains"
-rm -rf toolchain out AnyKernel
-echo "cleaned up"
-echo "Cloning toolchain"
-git clone --depth=1 https://github.com/mvaisakh/gcc-arm64 gcc-64
-echo "Done"
-if [ "$is_test" = true ]; then
-     echo "Its alpha test build"
-     unset chat_id
-     unset token
-     export chat_id=${my_id}
-     export token=${nToken}
-else
-     echo "Its beta release build"
-fi
-SHA=$(echo $DRONE_COMMIT_SHA | cut -c 1-8)
+# integrate kernelsu-next
+curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
+
+clang() {
+    rm -rf clang
+    echo "Cloning clang"
+    if [ ! -d "clang" ]; then
+        		git clone --depth=1 https://gitlab.com/itsshashanksp/android_prebuilts_clang_host_linux-x86_clang-r547379.git --depth=1 clang
+        KBUILD_COMPILER_STRING="Aosp clang 20.0"
+        PATH="${PWD}/clang/bin:${PATH}"
+    fi
+    sudo apt install -y ccache
+    echo "Done"
+}
+
 IMAGE=$(pwd)/out/arch/arm64/boot/Image.gz-dtb
-DATE=$(date +'%H%M-%d%m%y')
+DATE=$(date +"%Y%m%d-%H%M")
 START=$(date +"%s")
-CODENAME=mido
-DEF=teletubies_defconfig
-export CROSS_COMPILE="$(pwd)/gcc-64/bin/aarch64-elf-"
-export PATH="$(pwd)/gcc-64/bin:$PATH"
-export ARCH=arm64
-export KBUILD_BUILD_USER=malkist
-export KBUILD_BUILD_HOST=android
+KERNEL_DIR=$(pwd)
+CACHE=1
+export CACHE
+export KBUILD_COMPILER_STRING
+ARCH=arm64
+export ARCH
+KBUILD_BUILD_HOST="android"
+export KBUILD_BUILD_HOST
+KBUILD_BUILD_USER="malkist"
+export KBUILD_BUILD_USER
+DEVICE="Redmi Note 4"
+export DEVICE
+CODENAME="mido"
+export CODENAME
+# DEFCONFIG=""
+#DEFCONFIG_COMMON="vendor/msm8953-romi_defconfig"
+DEFCONFIG_DEVICE="teletubies_defconfig"
+#export DEFCONFIG_COMMON
+export DEFCONFIG_DEVICE
+COMMIT_HASH=$(git rev-parse --short HEAD)
+export COMMIT_HASH
+PROCS=$(nproc --all)
+export PROCS
+STATUS=STABLE
+export STATUS
+source "${HOME}"/.bashrc && source "${HOME}"/.profile
+if [ $CACHE = 1 ]; then
+    ccache -M 100G
+    export USE_CCACHE=1
+fi
+LC_ALL=C
+export LC_ALL
+
+tg() {
+    curl -sX POST https://api.telegram.org/bot"${token}"/sendMessage -d chat_id="${chat_id}" -d parse_mode=Markdown -d disable_web_page_preview=true -d text="$1" &>/dev/null
+}
+
+tgs() {
+    MD5=$(md5sum "$1" | cut -d' ' -f1)
+    curl -fsSL -X POST -F document=@"$1" https://api.telegram.org/bot"${token}"/sendDocument \
+        -F "chat_id=${chat_id}" \
+        -F "parse_mode=Markdown" \
+        -F "caption=$2 | *MD5*: \`$MD5\`"
+}
+
+# Send Build Info
+sendinfo() {
+    tg "
+• sirCompiler Action •
+*Building on*: \`Github actions\`
+*Date*: \`${DATE}\`
+*Device*: \`${DEVICE} (${CODENAME})\`
+*Branch*: \`$(git rev-parse --abbrev-ref HEAD)\`
+*Last Commit*: [${COMMIT_HASH}](${REPO}/commit/${COMMIT_HASH})
+*Compiler*: \`${KBUILD_COMPILER_STRING}\`
+*Build Status*: \`${STATUS}\`"
+}
+
 # Push kernel to channel
-function push() {
+push() {
     cd AnyKernel || exit 1
     ZIP=$(echo *.zip)
-    curl -F document=@$ZIP "https://api.telegram.org/bot$token/sendDocument" \
-        -F chat_id="$chat_id" \
-        -F "disable_web_page_preview=true" \
-        -F "parse_mode=html" \
-        -F caption="Build took $(($DIFF / 60)) minute(s) and $(($DIFF % 60)) second(s). | For <b>Samsung J6+</b>"
+    tgs "${ZIP}" "Build took $((DIFF / 60)) minute(s) and $((DIFF % 60)) second(s). | For *${DEVICE} (${CODENAME})* | ${KBUILD_COMPILER_STRING}"
 }
-# Compile plox
-function compile() {
-	make -j$(nproc --all) O=out 
-     make -j $(nproc --all) O=out ${DEF}
-     CONFIG_DEBUG_SECTION_MISMATCH=y 
-	CONFIG_NO_ERROR_ON_MISMATCH=y   2>&1 | tee error.log     
 
-     if ! [ -a "$IMAGE" ]; then
+# Catch Error
+finderr() {
+    curl -s -X POST "https://api.telegram.org/bot$token/sendMessage" \
+        -d chat_id="$chat_id" \
+        -d "disable_web_page_preview=true" \
+        -d "parse_mode=markdown" \
+        -d text="Build throw an error(s)"
+    exit 1
+}
+
+# Compile
+compile() {
+
+    if [ -d "out" ]; then
+        rm -rf out && mkdir -p out
+    fi
+
+    make O=out ARCH="${ARCH}"
+    make "$DEFCONFIG_COMMON" O=out
+    make "$DEFCONFIG_DEVICE" O=out
+    make -j"${PROCS}" O=out \
+        ARCH=$ARCH \
+        LLVM=1 \
+        LLVM_IAS=1 \
+        AR=llvm-ar \
+        NM=llvm-nm \
+        LD=ld.lld \
+        OBJCOPY=llvm-objcopy \
+        OBJDUMP=llvm-objdump \
+        STRIP=llvm-strip \
+        CC=clang \
+        CLANG_TRIPLE=aarch64-linux-gnu- \
+        CROSS_COMPILE=aarch64-linux-android- \
+	    CROSS_COMPILE_ARM32=arm-linux-gnueabi- \
+        CONFIG_DEBUG_SECTION_MISMATCH=y \
+	    CONFIG_NO_ERROR_ON_MISMATCH=y   2>&1 | tee error.log
+
+    if ! [ -a "$IMAGE" ]; then
         finderr
         exit 1
-     fi
+    fi
+
     git clone --depth=1 https://github.com/malkist01/anykernel3.git AnyKernel -b master
     cp out/arch/arm64/boot/Image.gz-dtb AnyKernel
 }
 # Zipping
 zipping() {
     cd AnyKernel || exit 1
-    zip -r9 Teletubies-"${CODENAME}"-Arm64"${DATE}".zip ./*
+    zip -r9 kernel-testing-"${BRANCH}"-"${CODENAME}"-"${DATE}".zip ./*
     cd ..
 }
+
+clang
+sendinfo
 compile
 zipping
 END=$(date +"%s")
-DIFF=$(($END - $START))
+DIFF=$((END - START))
 push
